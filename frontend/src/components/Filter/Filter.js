3c9eeded7937e66b5e654f7e806032e95a4155d7
DmitrijKl <klyanchenkov.dmitrij@.mail.ru>
2024-03-14 18:35:06 +0400

    Added BookForm,BookList and Filter components

---
+++ frontend/src/components/Filter/Filter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Filter.js";
+
+function Filter() {
+  return (
+    <div className="app-block filter">
+      <h2>Filters</h2>
+    </div>
+  );
+}
+
+export default Filter;
