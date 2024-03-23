35c9faf061c492ddefee8c07733f516a9dcf8dc9
DmitrijKl <klyanchenkov.dmitrij@.mail.ru>
2024-03-23 12:41:15 +0400

    Added errorSlice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import booksSlice from "./slices/booksSlice";
 import filterSlice from "./slices/filterSlice";
+import errorReducer from "./slices/errorSlice";
 
 const store = configureStore({
-  reducer: { booksSlice, filter: filterSlice },
+  reducer: { booksSlice, filter: filterSlice, error: errorReducer },
 });
 
 export default store;
